b7094cad7e1b2e9c7b4be1c51588454cb90abc7a
JuanCMarinC <fcecepjuancam@gmail.com>
2023-09-23 11:43:02 -0500

    funciones con promera y sync

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const util = require('util');
 const readline = require('readline-sync');
 
 let tareas2 = [
@@ -11,71 +13,94 @@ let tareas2 = [
         descripcion: 'Leer la biblia',
         estado: false
     }
+];
 
-]
+function encontrarMayorID() {
+    const mayorID = tareas2.reduce((maxID, tarea) => {
+      return tarea.id > maxID ? tarea.id : maxID;
+    }, -1);
+  
+    if (mayorID >= 0) {
+      return mayorID; // Retorna el mayor ID encontrado
+    } else {
+      return 1; // no hay tareas arrancamos con id 1
+    }
+  }
+
+function addTarea(terea) {
+    return new Promise((resolve, reject) => {
+        tareas2.push(terea);
+        resolve();
+    });
+}
 
 
 
 
 
-function addTarea(terea){tareas2.push(terea);}
-
-
-
-function listarTareas (){
+function listarTareas() {
     let contador = 0;
-    let mensaje="";
-    while(contador<tareas2.length){
-        if(tareas2[contador].estado==false){
-            mensaje = mensaje+"ID tarea = "+tareas2[contador].id+"  Descripcion = "+tareas2[contador].descripcion+
-                     "  Estado = Pendiente \n";
-        }else{
-            mensaje = mensaje+"ID tarea = "+tareas2[contador].id+"  Descripcion = "+tareas2[contador].descripcion+
-                     "  Estado = Terminada \n"
-         }
-    contador++;
+    let mensaje = "";
+    while (contador < tareas2.length) {
+        if (tareas2[contador].estado == false) {
+            mensaje = mensaje + "ID tarea = " + tareas2[contador].id + "  Descripcion = " + tareas2[contador].descripcion +
+                "  Estado = Pendiente \n";
+        } else {
+            mensaje = mensaje + "ID tarea = " + tareas2[contador].id + "  Descripcion = " + tareas2[contador].descripcion +
+                "  Estado = Terminada \n";
+        }
+        contador++;
     }
     return mensaje;
 }
 
-function agregarT(){
-    let cont =1;
-    for (let i = 0; i < cont; i++) {
+function agregarT() {
+    return new Promise((resolve, reject) => {
+        let cont = 1;
 
-       const tereaTemp={
-            id: tareas2.length+1,
-            descripcion: readline.question('Ingrese la descripcion de la nueva actividad \n'),
-            estado: false
-        }                
-        addTarea(tereaTemp);
-        let add=readline.question('Desea AGREGAR otra tarea ? : s/n \n');
-        if(add =="n" || add == "N"){break;}
-        cont++;
-      }   
-   menuP();
-}
+        
 
-function eliminarT(){
-    let ide1=readline.question('Digite el ID de la tarea a ELIMINAR :');
-    tareas2=tareas2.filter(tareas=> tareas.id != ide1);
-    menuP();
-};
-
-function completarT(){
-    let ide=readline.question('Digite el ID de la tarea a COMPLETAR :');
-    let contador = 0;
-    while(contador<tareas2.length){
-        if(tareas2[contador].id == ide){
-            tareas2[contador].estado = true;           
+        for (let i = 0; i < cont; i++) {
+            const tereaTemp = {
+                id: Number(encontrarMayorID())+1,
+                descripcion: readline.question('Ingrese la descripcion de la nueva actividad \n'),
+                estado: false
+            };
+            addTarea(tereaTemp)
+                    let add = readline.question('Desea AGREGAR otra tarea ? : s/n \n');
+                    if (add.toLowerCase() === 'n') {
+                        resolve();
+                    } else {
+                        cont++;
+                    }
+               
         }
-
-        contador ++;
-    }
-    menuP();
-
+    });
 }
 
-function menuP(){
+function eliminarT() {
+    return new Promise((resolve, reject) => {
+        let ide1 = readline.question('Digite el ID de la tarea a ELIMINAR :');
+        tareas2 = tareas2.filter(tareas => tareas.id != ide1);
+        resolve();
+    });
+}
+
+function completarT() {
+    return new Promise((resolve, reject) => {
+        let ide = readline.question('Digite el ID de la tarea a COMPLETAR :');
+        let contador = 0;
+        while (contador < tareas2.length) {
+            if (tareas2[contador].id == ide) {
+                tareas2[contador].estado = true;
+            }
+            contador++;
+        }
+        resolve();
+    });
+}
+
+function menuP() {
     console.clear();
     console.log('===========================================');
     console.log('==========Lista de tareas==================');
@@ -83,26 +108,49 @@ function menuP(){
 
     let listaT = listarTareas();
     console.log(listaT);
-    
+
     console.log('===========================================');
     console.log('==========Seleccione una accion============');
     console.log('===========================================');
-    console.log('    1. '+'Agregar Tarea');
-    console.log('    2. '+'Completar Tarea');
-    console.log('    3. '+'Eliminar Tarea');
+    console.log('    1. ' + 'Agregar Tarea');
+    console.log('    2. ' + 'Completar Tarea');
+    console.log('    3. ' + 'Eliminar Tarea');
     console.log('===========================================');
     console.log('===========================================');
-    
-    
-    let ans=readline.question('Digite el numero de la accion que desea realizar :');
 
-    if(ans==1){agregarT();}
-    else if(ans==2){completarT();}
-    else if(ans==3){eliminarT();}
-    else{}
-      
+    let ans = readline.question('Digite el numero de la accion que desea realizar :');
 
-
+    if (ans == 1) {
+        agregarT()
+            .then(() => {
+                console.log('Tareas agregadas exitosamente');
+                menuP();
+            })
+            .catch((error) => {
+                console.error('Error al agregar tareas:', error);
+            });
+    } else if (ans == 2) {
+        completarT()
+            .then(() => {
+                console.log('Tarea completada exitosamente');
+                menuP();
+            })
+            .catch((error) => {
+                console.error('Error al completar la tarea:', error);
+            });
+    } else if (ans == 3) {
+        eliminarT()
+            .then(() => {
+                console.log('Tarea eliminada exitosamente');
+                menuP();
+            })
+            .catch((error) => {
+                console.error('Error al eliminar la tarea:', error);
+            });
+    } else {
+        console.error('Error accion no valida :', error);
+        menuP();
+    }
 }
 
 menuP();
